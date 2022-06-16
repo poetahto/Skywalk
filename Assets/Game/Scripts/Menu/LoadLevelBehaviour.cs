a3aff9130b773fd519ac7378fa643a414d7e7930
Daniel Walls <walls.dk@gmail.com>
2022-06-16 11:58:30 -0700

    work on main menu

---
+++ Assets/Game/Scripts/Menu/LoadLevelBehaviour.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Game.Scripts
+{
+    public class LoadLevelBehaviour : MonoBehaviour
+    {
+        public void Test(string sceneName)
+        {
+            SceneTransitionSystem.Instance.TransitionToScene(sceneName);
+        }
+    }
+}
---
+++ Assets/Game/Scripts/Menu/QuitGameBehaviour.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Game.Scripts
+{
+    public class QuitGameBehaviour : MonoBehaviour
+    {
+        public void QuitGame()
+        {
+            Application.Quit();
+        }
+    }
+}
